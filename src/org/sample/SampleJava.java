a07a45500525f69f0d9e9f8982dd435ed1898b4e
AravinthMahalingam <aravinth06ms@gmail.com>
2025-01-27 20:17:43 +0530

    Build

---
+++ src/org/sample/SampleJava.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		
+		int b =20;
+		
+		int c = a+b;
+		
+		System.out.println(c);
+		
+		
+	}
+
+}
